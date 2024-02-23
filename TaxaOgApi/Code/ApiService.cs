e610d23245564b1b57054719548bc353c1303ea6
azak <azak@PROELE79>
2024-02-23 14:10:59 +0100

    l

--- TaxaOgApi/Code/ApiService.cs
+++ TaxaOgApi/Code/ApiService.cs
@@ -14,13 +14,13 @@ public class ApiService
 
     public async Task<string> GetDistance(string addressStart, string addressEnd)
     {
-        
-            string apiKey = "AIzaSyAi9pU1OmMqpvh4vYZ8udkJZ4ZyKh1aBiY";
-            string apiUrl = $"https://maps.googleapis.com/maps/api/distancematrix/json?origins={Uri.EscapeDataString(addressStart)}&destinations={Uri.EscapeDataString(addressEnd)}&key={apiKey}";
 
-            var response = await _httpClient.GetStringAsync(apiUrl);
-            Console.WriteLine($"API Response: {response}");
-            return response;
-        
+        string apiKey = "AIzaSyAi9pU1OmMqpvh4vYZ8udkJZ4ZyKh1aBiY";
+        string apiUrl = $"https://maps.googleapis.com/maps/api/distancematrix/json?origins={Uri.EscapeDataString(addressStart)}&destinations={Uri.EscapeDataString(addressEnd)}&key={apiKey}";
+
+        var response = await _httpClient.GetStringAsync(apiUrl);
+        Console.WriteLine($"API Response: {response}");
+        return response;
+
     }
 }
